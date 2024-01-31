2f0baec21ab750ad3d84b3067edf8b8fd48f6d75
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-01-31 15:15:15 +0530

    Time: 1 ms (100.00%), Space: 53.7 MB (32.69%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+    int[] result = new int[n];
+
+    // Compute left prefix product
+    int leftProduct = 1;
+    for (int i = 0; i < n; i++) {
+        result[i] = leftProduct;
+        leftProduct *= nums[i];
+    }
+
+    // Compute right suffix product
+    int rightProduct = 1;
+    for (int i = n - 1; i >= 0; i--) {
+        result[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    return result;
+    }
+}
