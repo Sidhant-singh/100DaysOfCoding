899aa46e4413246e83f7d828b2cb601fae0aa6d5
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-02-08 18:18:03 +0530

    Time: 32 ms (71.62%), Space: 42.3 MB (85.99%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int numSquares(int n) {
+        PerfectSquares solution = new PerfectSquares();
+        return solution.numSquares(n);
+    }
+
+    public class PerfectSquares {
+        public int numSquares(int n) {
+            int[] dp = new int[n + 1];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] = 0;
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j * j <= i; j++) {
+                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                }
+            }
+            return dp[n];
+        }
+    }
+}
