b55d9c5325da28ee0db3a65dc525155986ea5f73
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-07-10 02:40:18 +0530

    Time: 1 ms (86.70%), Space: 40.2 MB (96.49%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,26 +1,15 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) {
-            return x;
-        }
-        
-        long start = 1;
-        long end = x;
-        long ans = 0;
-        
-        while (start <= end) {
-            long mid = start + (end - start) / 2;
-            
-            if (mid * mid == x) {
+        long low = 0;
+        long high = x;
+        while(low<=high){
+            long mid = low + (high-low)/2;
+            if(mid*mid==(long)x){
                 return (int)mid;
-            } else if (mid * mid < x) {
-                start = mid + 1;
-                ans = mid; // Update the potential answer
-            } else {
-                end = mid - 1;
             }
+            else if(mid*mid<x) low = mid + 1;
+            else high = mid - 1;
         }
-        
-        return (int)ans;
+        return (int)high;
     }
 }
