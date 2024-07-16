ea6e840f7baed230e724c42edd725a51d8bb1626
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-07-16 17:38:39 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (25.89%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,37 +10,36 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        // If the list is empty or has only one node, return the head
-        if (head == null || head.next == null)
+        if(head==null || head.next==null){
             return head;
-
-        // Calculate the length of the list and find the tail node
-        int length = 1;
-        ListNode tail = head;
-        while (tail.next != null) {
-            tail = tail.next;
-            length++;
         }
-
-        // Calculate the actual rotation amount
-        k = k % length;
-
-        // If k is 0, no rotation needed, return the head
-        if (k == 0)
-            return head;
-
-        // Find the new tail node
-        int newTailPosition = length - k;
-        ListNode newTail = head;
-        for (int i = 1; i < newTailPosition; i++) {
-            newTail = newTail.next;
+//         finding the length
+        ListNode temp = head;
+        int len = 0;
+        while(temp!=null){
+            temp = temp.next;
+            len++;
         }
-
-        // Adjust pointers to rotate the list
-        ListNode newHead = newTail.next;
-        newTail.next = null;
-        tail.next = head;
-
-        return newHead;
+        ListNode dummy = head;
+        ListNode slow = head;
+        ListNode fast = head;
+        k=k%len;
+        if(k==0) return head;
+        for(int i=1;i<=k;i++){
+            fast = fast.next;
+        }
+        
+        while(fast.next!=null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        ListNode newNode = slow.next;
+        slow.next = null;
+        fast.next = head;
+        
+        return newNode;
+        
+        
     }
 }
