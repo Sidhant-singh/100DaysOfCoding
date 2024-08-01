ee2b6548e43abd52910ae12c3b52c5ca80e02745
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-08-01 19:34:43 +0530

    Time: 3 ms (55.90%), Space: 42.6 MB (98.24%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -7,7 +7,10 @@ class Solution {
         
         int i = 0, j = 0;
         int k = 0;
-        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+        if(nums1.length>nums2.length){
+            int []temp = new int[nums2.length];
+        }
+        int []temp = new int[nums1.length];
         
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i] == nums2[j]) {
@@ -22,7 +25,6 @@ class Solution {
                 j++;
             }
         }
-        
         return Arrays.copyOf(temp, k);
     }
 }
