d20f09293204e19afba5e72bdb4869e1e5223693
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-08-14 01:38:35 +0530

    Time: 17 ms (19.33%), Space: 44.5 MB (44.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,30 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char S[] = s.toCharArray();
-        char T[] = t.toCharArray();
-        if(S.length!=T.length){
-            return false;
-        }
-        Arrays.sort(S);
-        Arrays.sort(T);
-        for(int i=0;i<S.length;i++){
-            if(S[i]!=T[i]){
-                return false;
+        if(s.length()!=t.length()) return false;
+        HashMap<Character,Integer> smap = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char key = s.charAt(i);
+            if(smap.containsKey(key)){
+                int freq = smap.get(key);
+                smap.put(key,freq+1);
             }
+            else smap.put(key,1);
+        }
+        HashMap<Character,Integer> tmap = new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            char key = t.charAt(i);
+            if(tmap.containsKey(key)){
+                int freq = tmap.get(key);
+                tmap.put(key,freq+1);
+            }
+            else tmap.put(key,1);
+        }
+        
+        for(char key : smap.keySet()){
+            int val1 = smap.get(key);
+            if(!tmap.containsKey(key)) return false;
+            int val2 = tmap.get(key);
+            if(val1!=val2) return false;
         }
         return true;
     }
