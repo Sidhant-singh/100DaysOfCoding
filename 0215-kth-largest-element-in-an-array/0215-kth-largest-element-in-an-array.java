2f11bd87d24a7af3031a6ea8ddc8ad5258ba21d8
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-08-28 20:01:45 +0530

    Time: 61 ms (44.99%), Space: 61.8 MB (8.33%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,51 +1,11 @@
 class Solution {
-    static int ans;
-    
-    public void swap(int []arr,int i,int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    public int partition(int []arr,int low,int high){
-        int pivot = arr[low];
-        int smallerCount=0;
-        for(int i=low+1;i<=high;i++){
-            if(arr[i]<=pivot) smallerCount++;
-        }
-        
-        int correctIdx = low + smallerCount;
-        
-        swap(arr,correctIdx,low);
-        
-        int i=low;
-        int j=high;
-        
-        while(i<correctIdx && j>correctIdx){
-            if(arr[i]<=pivot) i++;
-            else if(arr[j]>pivot) j--;
-            else if(arr[i]>pivot && arr[j]<=pivot){
-                swap(arr,i,j);
-            }
-        }
-        
-        return correctIdx;
-    }
-    public void quickSelect(int []arr,int low,int high,int k){
-        if(low>high) return;
-        
-        int idx = partition(arr,low,high);
-        
-        if(idx==k-1){
-            ans = arr[idx];
-            return;
-        }
-        if(k-1<idx) quickSelect(arr,low,idx-1,k);
-        else quickSelect(arr,idx+1,high,k);
-    }
     public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
-        ans = -1;
-        quickSelect(nums,0,n-1,n-k+1);
-        return ans;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int ele : nums){
+            pq.add(ele);
+            if(pq.size()>k) pq.remove();
+        }
+        
+        return pq.peek();
     }
 }
