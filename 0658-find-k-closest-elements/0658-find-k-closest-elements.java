c6d26bc6e566e2bf1551fcfdab3f36cdccc2363f
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-08-28 23:46:58 +0530

    Time: 55 ms (5.52%), Space: 45.9 MB (42.01%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,70 +1,30 @@
 class Solution {
+   public class pair implements Comparable<pair>{
+        int ele;
+        int diff;
+        pair(int ele,int diff){
+            this.ele = ele;
+            this.diff = diff;
+        }
+        public int compareTo(pair p){
+            if(this.diff==p.diff)
+                return this.ele - p.ele;
+            return this.diff - p.diff;
+        }
+    }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> res = new ArrayList<>();
-        int n = arr.length;
-//         if the target is less than the first element then we have to return first k elements
-        if(x<arr[0]){
-            for(int i=0;i<k;i++){
-                res.add(arr[i]);
-            }
-            return res;
+        PriorityQueue<pair> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int ele:arr){
+            int diff = Math.abs(x-ele);
+            pq.add(new pair(ele,diff));
+            if(pq.size()>k) pq.remove();
         }
-//         if the target is more than the last element then we have to return last n-k elements
-        if(x>arr[n-1]){
-            for(int i=n-1;i>=n-k;i--){
-                res.add(arr[i]);
-            }
-            Collections.sort(res);
-            return res;
+        List<Integer> ans = new ArrayList<>();
+        while(pq.size()>0){
+            pair p = pq.remove();
+            ans.add(p.ele);
         }
-        
-//         finding the lower bound
-        int lb = n;
-        int low = 0;
-        int high = n-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(arr[mid]>=x){
-                lb = mid;
-                high=mid-1;
-            }
-            else low=mid+1;
-        }
-        
-//         placing the pointer wrt to lb
-        int i = lb-1;
-        int j = lb;
-        
-        while(i>=0 && j<n && k>0){
-            int di = Math.abs(x-arr[i]);
-            int dj = Math.abs(x-arr[j]);
-            if(di<=dj){
-                res.add(arr[i]);
-                i--;
-               
-            }
-            else{
-                res.add(arr[j]);
-                j++;
-                
-            }
-            k--;
-        }
-        
-//         now this can happen that there are some elements left and the pointers approaches to end
-        while(i<0 && k>0){
-            res.add(arr[j]);
-            j++;
-            k--;
-        }
-        
-        while(j==n && k>0){
-            res.add(arr[i]);
-            i--;
-            k--;
-        }
-        
-        Collections.sort(res);
-        return res;
+        Collections.sort(ans);
+        return ans;
     }
 }
