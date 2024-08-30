03cce804477f2ea9172d09ec0e2e74b95ae37709
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-08-30 17:49:56 +0530

    Time: 4 ms (99.97%), Space: 47.5 MB (81.23%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,20 +1,23 @@
 class Solution {
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k <= 1) return 0; // Since k > 1, product will always be greater than or equal to k
-
+    public int numSubarrayProductLessThanK(int[] nums, int target) {
+        if (target <= 1) return 0; // Edge case where no product can be less than 1
+        
+        int n = nums.length;
         int count = 0;
-        int product = 1;
-        int left = 0;
+        int pro = 1;
+        int i = 0;
 
-        for (int right = 0; right < nums.length; right++) {
-            product *= nums[right];
+        // Sliding window
+        for (int j = 0; j < n; j++) {
+            pro *= nums[j];
 
-            while (product >= k) {
-                product /= nums[left];
-                left++;
+            // Shrink the window if the product is greater than or equal to the target
+            while (pro >= target && i <= j) {
+                pro /= nums[i++];
             }
 
-            count += right - left + 1; // Number of valid subarrays ending at right
+            // All subarrays ending at j and starting from any index between i and j have a product less than target
+            count += j - i + 1;
         }
 
         return count;
