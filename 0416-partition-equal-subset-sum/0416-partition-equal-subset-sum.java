9f7168cd2e8a4be7bed4b3536f3a48587df140ea
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-09-14 16:32:17 +0530

    Time: 77 ms (33.39%), Space: 58 MB (13.91%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,28 +1,28 @@
 class Solution {
-    private boolean subset(int i,int []arr,int target,int [][]dp){
-        if(i<0){
-            if(target==0) return true;
-            else return false;
-        }
+//     private boolean subset(int i,int []arr,int target,int [][]dp){
+//         if(i<0){
+//             if(target==0) return true;
+//             else return false;
+//         }
 
-        if(dp[i][target]!=-1){
-            if(dp[i][target] == 1) return true;
-            else return false;
-        }
+//         if(dp[i][target]!=-1){
+//             if(dp[i][target] == 1) return true;
+//             else return false;
+//         }
 
-        boolean ans = false;
-        boolean skip = subset(i-1,arr,target,dp);
-        if(target-arr[i]<0){ //only valid for positive numbers
-            ans = skip;
-         }
-        else{
-            boolean pick = subset(i-1,arr,target-arr[i],dp);
-            ans = pick || skip;
-        }
-        if(ans) dp[i][target] = 1;
-        else dp[i][target] = 0;
-        return ans;
-    }
+//         boolean ans = false;
+//         boolean skip = subset(i-1,arr,target,dp);
+//         if(target-arr[i]<0){ //only valid for positive numbers
+//             ans = skip;
+//          }
+//         else{
+//             boolean pick = subset(i-1,arr,target-arr[i],dp);
+//             ans = pick || skip;
+//         }
+//         if(ans) dp[i][target] = 1;
+//         else dp[i][target] = 0;
+//         return ans;
+//     }
     
     public boolean canPartition(int[] nums) {
         int sum=0;
@@ -36,9 +36,18 @@ class Solution {
         int [][]dp = new int[nums.length][target+1];
         for(int i=0;i<dp.length;i++){
             for(int j=0;j<dp[0].length;j++){
-                dp[i][j] = -1;
+                boolean ans = false;
+                boolean skip = (i>0) ? (dp[i-1][j]==1) : (j==0);
+                if(j-nums[i]<0){ //only valid for positive numbers
+                    ans = skip;
+                 }
+                else{
+                    boolean pick = (i>0) ? (dp[i-1][j-nums[i]]==1) : (j==0);
+                    ans = pick || skip;
+                }
+                dp[i][j] = (ans) ? 1 : 0;
             }
         }
-        return subset(n-1,nums,target,dp);
+        return (dp[n-1][target]==1);
     }
 }
