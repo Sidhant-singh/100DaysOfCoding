4db67a2b335873362c400ea5a7f0fca49b17ac1f
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-09-14 15:48:44 +0530

    Time: 10 ms (60.40%), Space: 44.5 MB (32.43%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,16 +1,33 @@
 class Solution {
-    public int ways(int idx ,int[] nums, int target) {
+    static int sum;
+    public int ways(int idx ,int[] nums,int res, int target,int [][] dp) {
         if(idx==nums.length){
-            if(target==0) return 1;
+            if(res==target) return 1;
             else return 0;
         }
+        if(dp[idx][res+sum]!=-1) return dp[idx][res+sum]; 
+        int add = ways(idx+1,nums,res+nums[idx],target,dp);
+        int sub = ways(idx+1,nums,res-nums[idx],target,dp);
         
-        int add = ways(idx+1,nums,target-nums[idx]);
-        int sub = ways(idx+1,nums,target+nums[idx]);
-        
-        return add+sub;
+        return dp[idx][res+sum] = add+sub;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return ways(0,nums,target);
+        sum = 0;
+        int n=nums.length;
+        for(int ele : nums){
+            sum += ele;
+        }
+//         i - 0 to n-1 || target -> -sum to +sum
+//         dp[i][target+sum] -> ways(i,target)
+//         in some cases the target of the value of goes in negative and we have to 
+//         store the negative values and the array can't store negative values 
+//         so we have taken 2*sum + 1 as target range is always -sum to +sum
+        int [][] dp = new int[n][2*sum+1];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j] = -1;
+            }
+        }
+        return ways(0,nums,0,target,dp);
     }
 }
