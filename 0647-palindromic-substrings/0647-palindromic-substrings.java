11844f72371b831c0d499699164b8e358f1a1070
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-09-15 19:05:56 +0530

    Time: 17 ms (39.20%), Space: 49.2 MB (8.27%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,32 +1,30 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n = s.length();
-        int count = 0;
-        // Create a 2D array to store whether substring i to j is a palindrome
-        boolean[][] dp = new boolean[n][n];
-        
-        // Each individual character is a palindrome
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-            count++;
-        }
-        
-        // Check for palindromic substrings of length 2
-        for (int i = 0; i < n - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                count++;
-            }
-        }
-        
-        // Check for palindromic substrings of length greater than 2
-        for (int length = 3; length <= n; length++) {
-            for (int i = 0; i <= n - length; i++) {
-                int j = i + length - 1;
-                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
+        int n = s.length(),count=0;
+        int [][]dp = new int[n][n];
+        for(int k=0;k<n;k++){
+            int i = 0,j=k;
+            while(j<n){
+                if(i==j){
+                    dp[i][j]=1;
                     count++;
                 }
+//                 for 2 length substring
+                else if(j==i+1){
+                    if(s.charAt(i)==s.charAt(j)){
+                        dp[i][j] = 1;
+                        count++;
+                    }
+                }
+                else{
+                    if(s.charAt(i)==s.charAt(j)){
+                        if(dp[i+1][j-1]==1){
+                            dp[i][j] = 1;
+                            count++;
+                        }
+                    }
+                }
+                i++;j++;
             }
         }
         
