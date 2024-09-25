25faf26eb52048f7b4ba41d5cfc963a49ba125c3
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-09-25 13:54:19 +0530

    Time: 5 ms (97.09%), Space: 43.6 MB (28.82%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,34 +1,21 @@
 class Solution {
-    
-    public boolean convert(int x ,int y){
-        String a = String.valueOf(x);
-        String b = String.valueOf(y);
-        
-        String ord1 = a+b;
-        String ord2 = b+a;
-        
-        return ord1.compareTo(ord2) < 0;
-    }
     public String largestNumber(int[] nums) {
         int n = nums.length;
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(convert(nums[j],nums[j+1])){
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                }
-            }
+        String []strNum = new String[n];
+        
+        for(int i=0;i<n;i++){
+            strNum[i] = String.valueOf(nums[i]);
         }
         
-        if(nums[0] == 0){
-            return "0";
-        }
+        Arrays.sort(strNum,(a,b)->(b+a).compareTo(a+b));
         
+        if (strNum[0].equals("0")) {
+                return "0";
+            }        
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<n;i++){
-            String k = String.valueOf(nums[i]);
-            sb.append(k);
+            String t = strNum[i];
+            sb.append(t);
         }
         
         return sb.toString();
