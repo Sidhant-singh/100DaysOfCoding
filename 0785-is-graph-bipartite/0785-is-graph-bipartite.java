07b0e068edb5f971b50b87e2ba7865b402bc99c9
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-10-28 12:59:08 +0530

    Time: 1 ms (66.60%), Space: 44.9 MB (52.70%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,43 +1,35 @@
-import java.util.*;
-
 class Solution {
     static boolean ans;
-    
-    public void bfs(int i, int[][] adj, int[] vis) {
+    public void bfs(int i,int [][]adj, int[]vis){
         Queue<Integer> q = new LinkedList<>();
-        vis[i] = 0; // Start coloring the node with color 0 (blue)
+        vis[i] = 0; // 1-> red && 0->blue
         q.add(i);
-        
-        while (!q.isEmpty()) {
-            int front = q.poll();
+        while(q.size()>0){
+            int front = q.remove();
             int color = vis[front];
-            
-            for (int neighbor : adj[front]) {
-                if (vis[neighbor] == color) {
-                    // If the neighboring node has the same color, it’s not bipartite
+            for(int ele : adj[front]){
+                if(vis[ele]==vis[front]){
                     ans = false;
                     return;
                 }
-                if (vis[neighbor] == -1) {
-                    // Color the neighbor with the opposite color
-                    vis[neighbor] = 1 - color;
-                    q.add(neighbor);
+                if(vis[ele]==-1){
+                    vis[ele] = 1-color;
+                    q.add(ele);
                 }
             }
         }
     }
-
     public boolean isBipartite(int[][] adj) {
         ans = true;
         int n = adj.length;
-        int[] vis = new int[n];
-        Arrays.fill(vis, -1); // Initialize all nodes as unvisited
-        
-        // Traverse each node, in case of disconnected graph components
-        for (int i = 0; i < n; i++) {
-            if (vis[i] == -1) { // If the node hasn't been visited
-                bfs(i, adj, vis);
-                if (!ans) return false; // Early return if a component is not bipartite
+        int []vis = new int[n];
+        Arrays.fill(vis,-1);
+        // 1->red & 0->blue 
+        for(int i=0;i<n;i++){
+            if(vis[i]==-1){
+                bfs(i,adj,vis);
+                if(ans == false) return ans;
+
             }
         }
         
