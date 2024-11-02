14020df8c80d559a5bf33fbf8934807777c1985c
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-11-02 13:04:25 +0530

    Time: 4 ms (90.49%), Space: 45.2 MB (53.79%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,44 +1,36 @@
 class Solution {
+    static boolean ans;
+    public void dfs(int i,List<List<Integer>> adj, boolean []vis,boolean []path){
+        vis[i] = true;
+        path[i] = true;
+        for(int ele : adj.get(i)){
+            if(path[ele]==true){
+                ans = false; //false means there is cycle
+                return ;
+            }
+            if(vis[ele]==false) dfs(ele,adj,vis,path);
+        }
+        path[i] = false;
+    }
     public boolean canFinish(int n, int[][] pre) {
+        ans = true; // true means no cycle
         List<List<Integer>> adj = new ArrayList<>();
         
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
         
-        int []indegree = new int[n];
-        boolean []visited = new boolean[n];
         
         for(int i=0;i<pre.length;i++){
             int a = pre[i][0];
             int b = pre[i][1];
             adj.get(b).add(a);
-            indegree[a]++;
         }
-        
-//         kahn's algorithm
-        Queue<Integer> q = new LinkedList<>();
-        List<Integer> ans = new ArrayList<>();
+        boolean []visited = new boolean[n];
+        boolean []path = new boolean[n];
         for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.add(i);
-                visited[i] = true;
-            }
+            dfs(i,adj,visited,path);
         }
-        
-        while(q.size()>0){
-            int front = q.poll();
-            ans.add(front);
-            for(int ele : adj.get(front)){
-                indegree[ele]--;
-                if(indegree[ele]==0){
-                    q.add(ele);
-                    visited[ele] = true;
-                }
-            }
-        }
-        
-        if(ans.size()==n) return true;
-        return false;
+        return ans;
     }
 }
