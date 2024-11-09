ed6f16a3b3068ba30e114ec619d87e3a1c41f7f4
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-11-09 15:37:05 +0530

    Time: 6 ms (87.56%), Space: 44.1 MB (73.46%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,23 +1,63 @@
 class Solution {
+    public class Pair {
+        int node;
+        int cost;
+        Pair(int node,int cost){
+            this.node = node;
+            this.cost = cost;
+        }
+    }
+
+    public class Triplet implements Comparable<Triplet>{
+        int node;
+        int cost;
+        int stops;
+        Triplet(int node,int cost,int stops){
+            this.node = node;
+            this.cost = cost;
+            this.stops = stops;
+        }
+
+        public int compareTo(Triplet t){
+            if(this.stops == t.stops) return this.cost - t.cost;
+            return this.stops - t.stops;
+        }
+    }
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        final int INF = 1_000_000_000;
-        int[] cost = new int[n];
-        Arrays.fill(cost, INF);
-        cost[src] = 0;
-        
-        for (int i = 0; i <= k; i++) {
-            int[] temp = Arrays.copyOf(cost, n);
-            for (int[] flight : flights) {
-                int from = flight[0];
-                int to = flight[1];
-                int price = flight[2];
-                if (cost[from] != INF && cost[from] + price < temp[to]) {
-                    temp[to] = cost[from] + price;
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+//        populating the list
+        for(int i=0;i<flights.length;i++){
+            int from = flights[i][0];
+            int to = flights[i][1];
+            int price = flights[i][2];
+            adj.get(from).add(new Pair(to,price));
+        }
+
+        int []ans = new int[n];
+        Arrays.fill(ans,Integer.MAX_VALUE);
+        ans[src] = 0;
+        PriorityQueue<Triplet> pq = new PriorityQueue<>();
+        pq.add(new Triplet(src,0,0));
+        while(pq.size()>0){
+            Triplet top = pq.poll();
+            int node = top.node;
+            int cost = top.cost;
+            int stops = top.stops;
+            //if(node==dst) return cost;
+            if(stops==k+1) continue;
+            for(Pair p: adj.get(node)){
+                int totalCost = cost + p.cost;
+                if(totalCost < ans[p.node]){
+                    ans[p.node] = totalCost;
+                    pq.add(new Triplet(p.node,totalCost,stops+1));
                 }
             }
-            cost = temp;
         }
-        
-        return cost[dst] == INF ? -1 : cost[dst];
+        if(ans[dst]==Integer.MAX_VALUE) return -1;
+        return ans[dst];
     }
 }
