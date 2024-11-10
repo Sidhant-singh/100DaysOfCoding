7cbd0d3a74c107798c3e28c6b7715cde79a48af8
Sidhant singh bhadauriya <123365953+Sidhant-singh@users.noreply.github.com>
2024-11-10 16:52:05 +0530

    Time: 2 ms (54.16%), Space: 47.9 MB (36.25%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,23 +1,42 @@
 class Solution {
-        static int []parent;
+       static int []parent;
+    static int []size;
     public int find(int a){
         if(parent[a]==a) return a;
-        return find(parent[a]);
+//        PATH COMPRESSION
+//        SECOND OPTIMIZATION -> TO MAKE IT A TWO LVL TREE so that it heights can be reduced nd make it a balanced tree
+//        so while we are finding the leader of any node so during back calls we will
+//        perform backtracking as it helps to only one parent of all the nodes present in that groups.
+        int leader = find(parent[a]);
+        parent[a] = leader;
+        return leader;
     }
 
-    public void union(int a,int b){
-        int leaderA = find(a);
-        int leaderB = find(b);
-        if(leaderA!=leaderB){
-            parent[leaderB] = leaderA;
+    public void union(int a,int b) {
+        a = find(a);
+        b = find(b);
+//        OPTIMIZATION DONE -> Earlier we are randomly making parent to one node to another but now we are
+//        we are comparing the sizes of both the nodes and then whose size is greater than we make it
+//        as a parent to other node.
+        if (a != b) {
+            if (size[a] > size[b]) { //a should be a parent of b
+                parent[b] = a;
+                size[a] += size[b];
+            } else {
+                parent[a] = b;
+                size[b] += size[a];
+            }
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         parent = new int[n+1]; // 1 to n nodes
+        size = new int[n+1];
 //        every node is a parent of itself
         for(int i=1;i<=n;i++){
             parent[i] = i;
+//            every node at the starting has a size 1 is they are individually scattered
+            size[i] = 1;
         }
         parent[0] = Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
@@ -35,5 +54,6 @@ class Solution {
 
         return count;
     }
+
 
 }
